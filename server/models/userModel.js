1166f8b404ea6245d54ba8616544aaf1996b9589
zhoupengkai <15556532221@163.com>
2018-03-07 21:38:46 +0800

    todo/:id query

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,6 +1,19 @@
 
 var mongoose = require('mongoose');
 
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+    email:{
+        required:true,
+        type:String,
+        trim:true,
+        minlength:1
+    },
+    name:String
+})
+
+/*
 var User = mongoose.model('User',{
     email:{
         required:true,
@@ -9,5 +22,8 @@ var User = mongoose.model('User',{
         minlength:1
     }
 });
+*/
+
+var User = mongoose.model('User',userSchema);
 
 module.exports = {User};
--- server/server.js
+++ server/server.js
@@ -12,6 +12,8 @@ var express = require('express');
 
 var bodyParser = require('body-parser');
 
+var {ObjectID} = require('mongodb');
+
 var app = express();
 
 var port = process.env.PORT || 3000;
@@ -57,6 +59,28 @@ app.get('/todos',(req,res)=>{
     })
 
 });
+
+app.get('/todos/:id',(req,res)=>{
+
+    var id = req.params.id;
+
+    if(!ObjectID.isValid(id)){
+        res.status(400).send('id is not valid');
+    }
+
+    Todo.findById(id).then((todo)=>{
+
+        if(!todo){
+            res.status(400).send(`${id} is exist`);
+        }
+
+        res.status(200).send({todo});
+
+    })
+
+
+
+})
 
 app.listen(port,()=>{
     console.log('starting node server port 3000');
